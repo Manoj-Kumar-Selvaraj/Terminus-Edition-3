6420cc3df384c62d0d13c1a913a7bdccaec84551
Manoj Kumar <ss.mano1998@gmail.com>
2026-08-13 13:05:58 +0530

    Add retrieval engine package

---
+++ .terminus/retrieval/__init__.py
@@ -0,0 +1,19 @@
+"""Policy-aware local retrieval engine for the Terminus control plane."""
+
+from .embeddings import HashingEmbedder
+from .engine import RetrievalEngine
+from .indexer import RepositoryIndexer
+from .models import InvocationContext, RetrievalQuery, SearchResult
+from .policy import RetrievalPolicy
+from .store import RetrievalStore
+
+__all__ = [
+    "HashingEmbedder",
+    "InvocationContext",
+    "RepositoryIndexer",
+    "RetrievalEngine",
+    "RetrievalPolicy",
+    "RetrievalQuery",
+    "RetrievalStore",
+    "SearchResult",
+]
