6d772c061d640a1d9179ba9a9033b6407f6d2537
Manoj Kumar <ss.mano1998@gmail.com>
2026-08-13 15:54:42 +0530

    Export dynamic evidence ingestor

--- .terminus/retrieval/__init__.py
+++ .terminus/retrieval/__init__.py
@@ -3,11 +3,13 @@
 from .embeddings import HashingEmbedder
 from .engine import RetrievalEngine
 from .indexer import RepositoryIndexer
+from .ingestion import DynamicEvidenceIngestor
 from .models import InvocationContext, RetrievalQuery, SearchResult
 from .policy import RetrievalPolicy
 from .store import RetrievalStore
 
 __all__ = [
+    "DynamicEvidenceIngestor",
     "HashingEmbedder",
     "InvocationContext",
     "RepositoryIndexer",
